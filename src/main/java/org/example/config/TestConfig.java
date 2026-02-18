6b294c8a448043f0260118648f6af80b3963a4aa
Dmitry029 <kdp_029@tut.by>
2026-02-18 18:09:03 +0300

    Add google_checks.xml

---
+++ src/main/java/org/example/config/TestConfig.java
@@ -0,0 +1,32 @@
+package org.example.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+    @Value("${browser}")
+    private String browser;
+
+    @Bean(destroyMethod = "quit")
+    public WebDriver webDriver() {
+        WebDriver driver = WebDriverFactory.create(browser);
+        WebDriverListener listener = new HighlightingListener();
+        return new EventFiringDecorator(listener).decorate(driver);
+    }
+
+    @Bean
+    public CatalogPage catalogPage(WebDriver driver, @Value("${base.url}") String baseUrl) {
+        return new CatalogPage(driver, baseUrl + "/catalog/courses");
+    }
+
+    @Bean
+    public CoursePage coursePage(WebDriver driver) {
+        return new CoursePage(driver);
+    }
+}
